30a351ae5f350204095b581eee3b5c0f286e6249
rafaeldellaquila <rafadellaquila@gmail.com>
2021-07-17 00:53:54 -0300

    :lipstick: setuping theme on app and change titles and descriptions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,20 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-
+import { ThemeProvider } from 'styled-components'
+import theme from 'styles/theme'
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>Boilerplate do Rafinha</title>
-        <meta
-          name="description"
-          content="A simple boilerplate for my new projects"
-        />
+        <title>Won Games</title>
+        <meta name="description" content="E-commerce game site" />
         <link rel="manifest" href="/public/manifest.json" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 export default App
