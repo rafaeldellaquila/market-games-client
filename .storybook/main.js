2d3b821281526baf3f39f7170e1e9c2875912e81
rafaeldellaquila <rafadellaquila@gmail.com>
2021-07-17 01:24:25 -0300

    :memo: update some sb config for relative paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-essentials",
-  ]
+  ],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
