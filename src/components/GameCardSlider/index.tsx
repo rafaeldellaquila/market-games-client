5abd200c1d629e5f916ff740ca4a3cbb19a68939
rafaeldellaquila <rafael@dellaquila.dev>
2022-09-05 16:23:37 -0300

    :lipstick: new slider for games

--- src/components/GameCardSlider/index.tsx
+++ src/components/GameCardSlider/index.tsx
@@ -6,12 +6,49 @@ import GameCard, { GameCardProps } from 'components/GameCard'
 import Slider, { SliderSettings } from 'components/Slider'
 import * as S from './styles'
 
-type GameCardSliderProps = {
+export type GameCardSliderProps = {
   items: GameCardProps[]
   color?: 'white' | 'black'
 }
 
-const settings: SliderSettings = {}
+const settings: SliderSettings = {
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  nextArrow: <ArrowRight aria-label="next games" />,
+  prevArrow: <ArrowLeft aria-label="previous games" />,
+
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2
+      }
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.2
+      }
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.1
+      }
+    }
+  ]
+}
 
 const GameCardSlider = ({ items, color = 'black' }: GameCardSliderProps) => (
   <S.Wrapper color={color}>
--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -1,13 +1,23 @@
-import { render, screen } from '@testing-library/react'
-
+import 'match-media-mock'
+import { renderWithTheme } from 'utils/tests/helpers'
+//import { screen } from '@testing-library/react'
+import items from './mock'
 import GameCardSlider from '.'
 
 describe('<GameCardSlider />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameCardSlider />)
-
-    expect(screen.getByRole('heading', { name: /GameCardSlider/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render game card slider with 4 items', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
   })
+
+  // it('should render the white arrows if color passed', () => {
+  //   renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+  //   expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+  //     color: '#FAFAFA'
+  //   })
+  //   expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+  //     color: '#FAFAFA'
+  //   })
+  // })
 })
